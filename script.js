3e1c717ec17dac2698d7123b64429658c20530e5
Moje Fotografie Rafał Sporakowski <diablo1sporak@gmail.com>
2025-10-08 19:27:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -120,3 +120,11 @@ document.addEventListener("DOMContentLoaded", () => {
     observer.observe(socialLinks);
   }
 });
+// Automatycznie dodaje "loading=lazy" do wszystkich zdjęć
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("img").forEach(img => {
+    if (!img.hasAttribute("loading")) {
+      img.setAttribute("loading", "lazy");
+    }
+  });
+});
